d04575baed2749fc1051a8ead7bef9c98dbd66e2
xZhad <isarpi@lemontech.com>
2021-01-12 19:04:49 -0300

    Brew formula update for metaq version v0.1.1

--- metaq.rb
+++ metaq.rb
@@ -2,16 +2,16 @@
 class Metaq < Formula
   desc "metaq propagates the same query on multiple databases on the same engine"
   homepage "https://github.com/LemontechSA/metaq"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/LemontechSA/metaq/releases/download/v0.1.0/metaq_0.1.0_Darwin_x86_64.tar.gz"
-    sha256 "4256a4579ca9210e2907ed2e58cbdc3b3131be8cf36c07585fa820c1d5c08cd8"
+    url "https://github.com/LemontechSA/metaq/releases/download/v0.1.1/metaq_0.1.1_Darwin_x86_64.tar.gz"
+    sha256 "b0bfcc74babe258a6469d86a9335768de23e8d30837e3d6cd1866d7156893f0f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/LemontechSA/metaq/releases/download/v0.1.0/metaq_0.1.0_Linux_x86_64.tar.gz"
-    sha256 "75020d1f72c6b74bbecfbb852691178be8a523a732136823f533a908aaa2d7b3"
+    url "https://github.com/LemontechSA/metaq/releases/download/v0.1.1/metaq_0.1.1_Linux_x86_64.tar.gz"
+    sha256 "6d9eadfe30aa2f1dbca5147f3c549a54c300dfd03b62520adf03d0e589fef4a4"
   end
 
   def install
